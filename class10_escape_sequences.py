2aee96a7731970d34eff5ca9443a30fac03f9122
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2022-11-28 11:22:46 +0530

    Add files via upload

---
+++ class10_escape_sequences.py
@@ -0,0 +1,8 @@
+'''escape sequences'''
+
+
+print("My name is\n Hardik Kumawat")
+print('This is Hardik\'s pencil')
+print("I am \\ very smart")
+print("Hardik \t Kumawat")
+print("Hell\bo")
---
+++ class11_comments.py
@@ -0,0 +1,11 @@
+'''this is a comment'''
+#this is also a comment
+
+print("Hardik\nkumawat")
+'''will give you the output
+
+
+        Hardik 
+        Kumawat '''
+
+#You can use the shortcut control+/ to turn an entire line into a comment!
