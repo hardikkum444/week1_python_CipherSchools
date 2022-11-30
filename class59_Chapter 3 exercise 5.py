e1c389e014820536571fed0996d0dcd0c0fbf7e0
Hardik Kumawat <114590291+hardikkum444@users.noreply.github.com>
2022-11-30 19:51:36 +0530

    Add files via upload

---
+++ class59_Chapter 3 exercise 5.py
@@ -0,0 +1,10 @@
+name=input('What is your name? ')
+
+temp_var=""
+i=0
+while i<len(name):
+    if name[i] not in temp_var:
+        temp_var += name[i]
+        print(f'#{name[i]} : {name.count(name[i])}')
+    i+=1
+
